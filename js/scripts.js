6b1d2a154ce15133ce3d3a9819a1e74aadbeab93
alexapenzes <penzesalexa@gmail.com>
2021-11-04 16:46:12 +0100

    Added a list of Pokemons
    array -> name, height, types

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert('Hello World');
-
-let favoriteFood = 'Sushi';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Charmander', height: 0.6, types: ['lizard', 'fire']},
+    {name: 'Jigglypuff', height: 0.5, types: ['balloon', 'fairy']},
+    {name: 'Growlithe', height: 0.7, types: ['puppy', 'fire']},
+    {name: 'Horsea', height: 0.4, types: ['dragon', 'water']},
+    {name: 'Oddish', height: 0.5, types: ['weed', 'grass', 'poison']},
+]
