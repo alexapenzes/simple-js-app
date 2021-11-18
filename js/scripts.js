f6466debf1ca3600e6c9eae389f00599e14ec396
alexapenzes <penzesalexa@gmail.com>
2021-11-18 21:26:22 +0100

    forEach() / IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,54 @@
-let pokemonList = [
-    {
-        name: 'Charmander', 
-        height: 0.6, 
-        types: ['lizard', 'fire']
-    },
-    {
-        name: 'Jigglypuff', 
-        height: 0.5, 
-        types: ['balloon', 'fairy']
-    },
-    {
-        name: 'Growlithe', 
-        height: 0.7, 
-        types: ['puppy', 'fire']
-    },
-    {
-        name: 'Rapidash', 
-        height: 1.7, 
-        types: ['horse', 'fire']
-    },
-    {
-        name: 'Horsea', 
-        height: 0.4, 
-        types: ['dragon', 'water']
-    },
-    {
-        name: 'Oddish', 
-        height: 0.5, 
-        types: ['weed', 'grass', 'poison']
-    },
-];
+//IIFE function
+let pokemonRepository = (function () {
+    //list of pokemon Array
+    let pokemonList = [
+        {
+            name: 'Charmander', 
+            height: 0.6, 
+            types: ['lizard', 'fire']
+        },
+        {
+            name: 'Jigglypuff', 
+            height: 0.5, 
+            types: ['balloon', 'fairy']
+        },
+        {
+            name: 'Growlithe', 
+            height: 0.7, 
+            types: ['puppy', 'fire']
+        },
+        {
+            name: 'Rapidash', 
+            height: 1.7, 
+            types: ['horse', 'fire']
+        },
+        {
+            name: 'Horsea', 
+            height: 0.4, 
+            types: ['dragon', 'water']
+        },
+        {
+            name: 'Oddish', 
+            height: 0.5, 
+            types: ['weed', 'grass', 'poison']
+        },
+    ];
 
-pokemonList.forEach(function(pokemon) {
-    document.write('<p id="pokedex-style">' + pokemon.name + ' ' + 'Height: ' + pokemon.height + 'm' + '</p>');
+    function getAll() {
+        return pokemonList;
+    }
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    };
+})();
+
+//write all pokemon with its infos
+pokemonRepository.getAll().forEach(function(pokemon) {
+    document.write('<p class="pokedex-style">' + pokemon.name + '<br>' + 'Height: ' + pokemon.height + 'm<br>' + 'Types: ' + pokemon.types + '</p>');
 });
